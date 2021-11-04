5f0b60a8c148f6cc91edfe07160af1ce8787087c
todd <todd.name>
2021-11-04 19:56:05 +0300

    add connect to db in application

--- cmd/main.go
+++ cmd/main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"log"
+	"os"
 
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 	"github.com/spf13/viper"
 	todo "github.com/todd-sudo/todo_app"
 	"github.com/todd-sudo/todo_app/pkg/handler"
@@ -15,7 +18,23 @@ func main() {
 		log.Fatalf("error initilizing configs: %s", err.Error())
 	}
 
-	repos := repository.NewRepository()
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("error loading env variables: %s", err.Error())
+	}
+
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		DBName:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("db.sslmode"),
+		Password: os.Getenv("DB_PASSWORD"),
+	})
+	if err != nil {
+		log.Fatalf("failed to initilizing db: %s", err.Error())
+	}
+
+	repos := repository.NewRepository(db)
 	services := service.NewService(*repos)
 	handlers := handler.NewHandler(services)
 
